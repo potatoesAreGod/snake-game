88288422a13091bc7c6d70b7557c780aa18c5872
potatoesAreGod <118043038+potatoesAreGod@users.noreply.github.com>
2023-11-26 19:23:48 +0100

    first commit

---
+++ Snake game V.1.py
@@ -0,0 +1,231 @@
+import random
+import pygame
+import tkinter as tk
+from tkinter import messagebox
+
+
+# klass för ormens kroppsdelar
+class cube(object):
+    rows = 20
+    w = 500
+
+    def __init__(self, start, dirnx=1, dirny=0, color=(255, 0, 0)):
+        self.pos = start
+        self.dirnx = 1
+        self.dirny = 0
+        self.color = color
+
+    def move(self, dirnx, dirny):
+        self.dirnx = dirnx
+        self.dirny = dirny
+        self.pos = (self.pos[0] + self.dirnx, self.pos[1] + self.dirny)
+
+    # rita kroppsdelarna på skärmen
+    def draw(self, surface, eyes=False):
+        dis = self.w // self.rows
+        i = self.pos[0]
+        j = self.pos[1]
+
+        pygame.draw.rect(surface, self.color, (i * dis + 1, j * dis + 1, dis - 2, dis - 2))
+        # rita ögon på ormens huvud
+        if eyes:
+            centre = dis // 2
+            radius = 3
+            circleMiddle = (i * dis + centre - radius, j * dis + 8)
+            circleMiddle2 = (i * dis + dis - radius * 2, j * dis + 8)
+            pygame.draw.circle(surface, (0, 0, 0), circleMiddle, radius)
+            pygame.draw.circle(surface, (0, 0, 0), circleMiddle2, radius)
+
+
+# klass för ormen
+class snake(object):
+    body = []
+    turns = {}
+
+    def __init__(self, color, pos):
+        self.color = color
+        self.head = cube(pos)
+        self.body.append(self.head)
+        self.dirnx = 0
+        self.dirny = 1
+
+    def move(self):
+        for event in pygame.event.get():
+            # avsluta spelet om användaren stänger fönsteret
+            if event.type == pygame.QUIT:
+                pygame.quit()
+
+            # registrerar när användaren trycker på tangentbordet
+            keys = pygame.key.get_pressed()
+
+            # itererar genom tangettryckningarna
+            for key in keys:
+                # om användaren trycker på en piltangent, ändra ormens riktning ditåt
+                if keys[pygame.K_LEFT]:
+                    self.dirnx = -1
+                    self.dirny = 0
+                    self.turns[self.head.pos[:]] = [self.dirnx, self.dirny]
+
+                elif keys[pygame.K_RIGHT]:
+                    self.dirnx = 1
+                    self.dirny = 0
+                    self.turns[self.head.pos[:]] = [self.dirnx, self.dirny]
+
+                elif keys[pygame.K_UP]:
+                    self.dirnx = 0
+                    self.dirny = -1
+                    self.turns[self.head.pos[:]] = [self.dirnx, self.dirny]
+
+                elif keys[pygame.K_DOWN]:
+                    self.dirnx = 0
+                    self.dirny = 1
+                    self.turns[self.head.pos[:]] = [self.dirnx, self.dirny]
+
+        # flyttar ormen ett steg framåt
+        for i, c in enumerate(self.body):
+            p = c.pos[:]
+            if p in self.turns:
+                turn = self.turns[p]
+                c.move(turn[0], turn[1])
+                if i == len(self.body) - 1:
+                    self.turns.pop(p)
+            else:
+                if c.dirnx == -1 and c.pos[0] <= 0:
+                    c.pos = (c.rows - 1, c.pos[1])
+                elif c.dirnx == 1 and c.pos[0] >= c.rows - 1:
+                    c.pos = (0, c.pos[1])
+                elif c.dirny == 1 and c.pos[1] >= c.rows - 1:
+                    c.pos = (c.pos[0], 0)
+                elif c.dirny == -1 and c.pos[1] <= 0:
+                    c.pos = (c.pos[0], c.rows - 1)
+                else:
+                    c.move(c.dirnx, c.dirny)
+
+    # återställer ormen ifall användaren vill spela igen
+    def reset(self, pos):
+        self.head = cube(pos)
+        self.body = []
+        self.body.append(self.head)
+        self.turns = {}
+        self.dirnx = 0
+        self.dirny = 1
+
+    # lägger till en kub i slutet på ormen när spelaren får ett poäng
+    def addCube(self):
+        tail = self.body[-1]
+        dx, dy = tail.dirnx, tail.dirny
+
+        if dx == 1 and dy == 0:
+            self.body.append(cube((tail.pos[0] - 1, tail.pos[1])))
+        elif dx == -1 and dy == 0:
+            self.body.append(cube((tail.pos[0] + 1, tail.pos[1])))
+        elif dx == 0 and dy == 1:
+            self.body.append(cube((tail.pos[0], tail.pos[1] - 1)))
+        elif dx == 0 and dy == -1:
+            self.body.append(cube((tail.pos[0], tail.pos[1] + 1)))
+
+        self.body[-1].dirnx = dx
+        self.body[-1].dirny = dy
+
+    # ritar ormen på skärmen
+    def draw(self, surface):
+        for i, c in enumerate(self.body):
+            if i == 0:
+                c.draw(surface, True)
+            else:
+                c.draw(surface)
+
+
+# ritar ett rutnät på skärmen
+def drawGrid(w, rows, surface):
+    sizeBtwn = w // rows
+    x = 0
+    y = 0
+
+    for _ in range(rows):
+        x = x + sizeBtwn
+        y = y + sizeBtwn
+
+        pygame.draw.line(surface, (230, 230, 230), (x, 0), (x, w))
+        pygame.draw.line(surface, (230, 230, 230), (0, y), (w, y))
+
+
+# uppdaterar fönstret
+def redrawWindow(surface):
+    global rows, width, s, snack
+    surface.fill((0, 0, 0))
+    s.draw(surface)
+    snack.draw(surface)
+    drawGrid(width, rows, surface)
+    pygame.display.update()
+
+
+# placerar ut ett "snack" på skärmen
+def randomSnack(rows, item):
+    positions = item.body
+
+    while True:
+        x = random.randrange(rows)
+        y = random.randrange(rows)
+        if len(list(filter(lambda z: z.pos == (x, y), positions))) > 0:
+            continue
+        else:
+            break
+
+    return (x, y)
+
+
+# för att skicka meddelanden till användaren, används för att fråga om användaren vill spela igen
+def message_box(subject, content):
+    root = tk.Tk()
+    root.attributes("-topmost", True)
+    root.withdraw()
+    messagebox.showinfo(subject, content)
+
+    try:
+        root.destroy()
+    except:
+        pass
+
+
+def main():
+    global width, rows, s, snack
+    # storlek på spelplanen
+    width = 500
+    rows = 20
+    win = pygame.display.set_mode((width, width))
+
+    # färg och startposition för ormen samt snack
+    s = snake((255, 0, 0), (10, 10))
+    snack = cube(randomSnack(rows, s), color=(0, 255, 0))
+
+    flag = True
+    clock = pygame.time.Clock()
+
+    while flag:
+        # sätt klockhastigheten
+        pygame.time.delay(50)
+        clock.tick(10)
+
+        # flytta ormen
+        s.move()
+
+        # om ormen äter ett snack
+        if s.body[0].pos == snack.pos:
+            s.addCube()
+            snack = cube(randomSnack(rows, s), color=(0, 255, 0))
+
+        # om ormen äter sig själv
+        for x in range(len(s.body)):
+            if s.body[x].pos in list(map(lambda z: z.pos, s.body[x + 1:])):
+                print('Poäng: ', len(s.body))
+                message_box('Du förlorade!', 'Spela igen...')
+                s.reset((10, 10))
+                break
+
+        # uppdatera fönsteret
+        redrawWindow(win)
+
+
+if __name__ == "__main__":
+    main()
